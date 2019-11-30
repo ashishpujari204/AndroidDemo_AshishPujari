cf43829f2a278ca935a6fa16cd40af3dea019b3c
Ashish Agatrao Pujari <ashishagatraopujari@Ashishs-MacBook-Air.local>
2019-11-30 14:58:15 +0530

    initial commit

---
+++ app/src/main/java/com/app/androiddemo_ashishpujari/MainActivity.java
@@ -0,0 +1,14 @@
+package com.app.androiddemo_ashishpujari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
